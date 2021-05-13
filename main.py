ff7cef6b85e994902ed52e3a9c2d7b686ebb3022
Jurrer <jk20@op.pl>
2021-05-13 16:11:17 +0200

    Commit message

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from GUI import Okno
-
 if __name__ == "__main__":
     apk = QApplication([])
 
     width, height = 1260, 680
-    tabs_app = Okno(width, height)
+    tabs_app = Main_Window(width, height)
 
     sys.exit(apk.exec_())
 
+    print("Hello World")
+    print("Ez")
+    print("Tomek update")
