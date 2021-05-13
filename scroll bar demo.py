819b64abbb5de9b58354a914a072e7f55a7de9ce
Jurrer <jk20@op.pl>
2021-05-13 16:16:56 +0200

    Commit message

--- scroll bar demo.py
+++ scroll bar demo.py
@@ -1,21 +1,19 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
+
+class EmptyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        width = 1260
+        height = 680
+        self.resize(width, height)
+        self.setWindowTitle("Empty Window")
+        self.show()
+
 if __name__ == "__main__":
-
-    class EmptyWindow(QWidget):
-        def __init__(self):
-            super().__init__()
-            width = 1260
-            height = 680
-            self.resize(width, height)
-            self.setWindowTitle("Empty Window")
-            self.show()
-
-    if __name__ == "__main__":
-        # print("Hello World")
-        # print("Ez")
-        app = QApplication([])
-
-        empty_wyswietl = EmptyWindow()
-        sys.exit(app.exec())
+    # print("Hello World")
+    # print("Ez")
+    app = QApplication([])
+    empty_wyswietl = EmptyWindow()
+    sys.exit(app.exec())
