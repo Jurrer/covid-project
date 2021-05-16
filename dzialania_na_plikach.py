67e796b2a5b377589eb49ab55d840e410b01a4e2
kapustekk <tomasz.kapuscinski.52@gmail.com>
2021-05-16 21:28:17 +0200

    Wczytanie pliku zrobione 21:27 16.05

--- dzialania_na_plikach.py
+++ dzialania_na_plikach.py
@@ -18,13 +18,48 @@ class WczytajPlik(QFileDialog):
     def __load_data(self, filename):
         countries_data = dict()
         column = 4
-
+        lista_krajow = list()
+        lista_pacjentow = list()
         with open(filename, "r") as f:
             for line in f:
                 country_name = line.split(",")[1]
-                if country_name != "Country/Region":
-                    line = line.strip()
-                    patients = line.split(",")[column:]
-                    countries_data[country_name] = patients
-
+                # print(country_name)
+                lista_krajow.append(country_name)
+                line = line.strip()
+                patients = line.split(",")[column:]
+                lista_pacjentow.append(patients)
+                # countries_data[country_name] = patients
+            countries_data = self.__sumuj_przebiegi(lista_krajow, lista_pacjentow)
+            country_names = list(countries_data.keys())
         print(countries_data)
+        print(country_names)
+
+    def __sumuj_przebiegi(self, names, values):
+        data = dict()
+        iterator = 1
+        country_value = list()
+        for name in names[1:]:
+            if iterator < len(names)-1:
+                next_name = names[iterator + 1]
+                if next_name == name:
+                    country_value = self.__sumuj_listy(country_value, values[iterator])
+                elif next_name != name:
+                    country_value = self.__sumuj_listy(country_value, values[iterator])
+                    data[name] = country_value
+                    country_value = list()
+                iterator += 1
+            else:
+                country_value = self.__sumuj_listy(country_value, values[iterator])
+                data[name] = country_value
+                country_value = list()
+        return data
+
+    def __sumuj_listy(self, listaa, listab):
+        value = list()
+        if len(listaa) > 0:
+            for i in range(len(listaa)):
+                v = int(listaa[i]) + int(listab[i])
+                value.append(v)
+        elif len(listaa) == 0:
+            value = listab
+        return value
